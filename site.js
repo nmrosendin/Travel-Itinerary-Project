fcc58d8187dcbf6df01ad26c38c6be7bd89410ea
Nicole <nicole.rosendin@gmail.com>
2015-10-02 17:13:43 -0700

    profile button goes to profile

--- site.js
+++ site.js
@@ -5,3 +5,11 @@ $(document).ready(function(){
         window.location.href="travel_submit_itinerary.html"
     });
 });
+
+$(document).ready(function(){
+
+    $("#myProfile").click(function(){
+        console.log("hi");
+        window.location.href="profile.html"
+    });
+});
