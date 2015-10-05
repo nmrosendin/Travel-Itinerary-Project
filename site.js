51d52dfc1118a642cbe32f7175e971876bc9a8fd
DEMI SKIPPER <Demiskipper@gmail.com>
2015-10-04 22:26:13 -0700

    attempting to get the FB Login working

--- site.js
+++ site.js
@@ -21,3 +21,23 @@ $(document).ready(function(){
         window.location.href="landingpage.html"
     });
 });
+
+$(document).ready(function(){
+
+    var ref = new Firebase("https://amber-heat-5381.firebaseio.com/");
+    ref.authWithOAuthPopup("facebook", function(error, authData) { d93713f82b19709de0f521137e0cab03 }, {
+        remember: "sessionOnly",
+        scope: "email,user_likes"
+    });
+
+    $("#FB").click(function(){
+        var ref = new Firebase("https://amber-heat-5381.firebaseio.com/");
+        ref.authWithOAuthPopup("facebook", function(error, authData) {
+            if (error) {
+                console.log("Login Failed!", error);
+            } else {
+                console.log("Authenticated successfully with payload:", authData);
+                    }
+            });
+        });
+    });
