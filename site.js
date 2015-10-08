6ce4df21c14557761c885f4984e03d7276e56911
Nicole <nicole.rosendin@gmail.com>
2015-10-07 18:30:21 -0700

    conflictfix

--- site.js
+++ site.js
@@ -15,19 +15,12 @@ $(document).ready(function(){
         window.location.href="landingpage.html"
     });
 
-<<<<<<< HEAD
-    // var ref = new Firebase("https://amber-heat-5381.firebaseio.com/");
-    // ref.authWithOAuthPopup("facebook", function(error, authData) { d93713f82b19709de0f521137e0cab03 }, {
-    //     remember: "sessionOnly",
-    //     scope: "email,user_likes"
-    // });
-=======
+
     $('#searchBox').click(function(e){
          e.preventDefault();
          var str = $('#search_location').val();
          console.log(str);
     });
->>>>>>> master
 
 // var str = $('#search_submit').val();
     // var ref = new Firebase("https://amber-heat-5381.firebaseio.com/");
